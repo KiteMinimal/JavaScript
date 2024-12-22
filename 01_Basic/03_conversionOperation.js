ff7ccbd503c020c97382015ac16d4d761624a8a1
KiteMinimal <mohdshoyeb189@gmail.com>
2024-12-22 14:57:09 +0530

    conversionOperation

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let scope = "33"
+// let scope = '44abc'
+// let scope = undefined
+
+// console.log(typeof scope)
+// console.log(typeof scope)
+
+// let valueInNumber = Number(scope)
+
+// console.log(typeof valueInNumber) // but here the value of valueInNumber is converted in number
+// console.log(valueInNumber) // In this the value of valueInNumber is NaN because js doesn't support this type of number like scope
+
+
+// 33 convert krte he to 33 
+// 33abc convert nhi hota
+
+let isLoggedin = ''
+let booleantype = Boolean(isLoggedin)
+console.log(booleantype)
+
+console.log(typeof booleantype)
+
+// 1 => true && 2 => false
+// "" => false
+// "shoyeb" => true
