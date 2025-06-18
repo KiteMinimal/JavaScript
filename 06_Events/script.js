85300f9481685d20f831879e325d3e47ce894539
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-06-18 20:10:52 +0530

    Refactor HTML and JavaScript: Comment out unused code and update heading text

--- 06_Events/script.js
+++ 06_Events/script.js
@@ -1,15 +1,33 @@
-document.getElementById("button")
-button.addEventListener("click", function(e){
-    e.stopPropagation()
-    console.log("clicked on button")
-})
+// document.getElementById("button")
+// button.addEventListener("click", function(e){
+//     e.stopPropagation()
+//     console.log("clicked on button")
+// })
 
-document.getElementById("images").addEventListener('click', (e) => {
-    console.log("clicked on image ul")
-})
+// document.getElementById("images").addEventListener('click', (e) => {
+//     console.log("clicked on image ul")
+// })
 
-document.getElementById("owl").addEventListener('click', (e) => {
-    console.log("clicked on owl")
-    e.stopPropagation()
-})
+// document.getElementById("owl").addEventListener('click', (e) => {
+//     console.log("clicked on owl")
+//     e.stopPropagation()
+// })
 
+const callMe = function () {
+  console.log("My Self Shoyeb Stack Developer");
+};
+
+const changeH1 = function () {
+  document.querySelector("h1").textContent = "Learning ReactJS";
+};
+const clearTime = setTimeout(() => {
+  callMe();
+  changeH1();
+}, 5000);
+
+
+const button = document.querySelector('#stop')
+
+button.addEventListener('click', () => {
+    clearTimeout(clearTime)
+})
