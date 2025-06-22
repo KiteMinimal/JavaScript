a19a98fd2ec0030c9a2b22a616f26250333628c5
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-06-22 09:40:55 +0530

    Implement User class with methods for encrypted password and capitalized username

---
+++ 12_Classes/classess.js
@@ -0,0 +1,46 @@
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptedPassword(){
+//         console.log(`${this.password}abc`)
+//         return 'encrypt';
+//     }
+
+//     capitalizedUserName(){
+//         console.log(`${this.username.toUpperCase()}`)
+//         return 'captial'
+//     }
+// }
+
+// const userCall = new User("kite", "kite@fb.com", "123")
+
+// console.log(userCall)
+// console.log(userCall.encryptedPassword())
+// const ep = userCall.encryptedPassword()
+// console.log(userCall.capitalizedUserName())
+// const cp = userCall.capitalizedUserName()
+
+function User(username, email, password){
+    this.username = username;
+    this.password = password;
+    this.email = email;
+}
+
+User.prototype.encryptedPassword = function(){
+    return(`${this.password}.abc`)
+}
+
+User.prototype.capitalizedUserName = function(){
+    return(`${this.username.toUpperCase()}`)
+}
+
+const detailUser = new User("kite", "kite@fb.com", "123")
+
+console.log(detailUser)
+
+console.log(detailUser.encryptedPassword())
+console.log(detailUser.capitalizedUserName())
